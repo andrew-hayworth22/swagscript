65498264bb7959ed3231efdc2084547893e8745c
andrew-hayworth22 <mhaywort@kent.edu>
2024-06-17 21:07:43 -0400

    Added builtin string length function

--- interpreter/object/object.go
+++ interpreter/object/object.go
@@ -17,6 +17,7 @@ const (
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
+	BUILTIN_OBJ      = "BUILTIN"
 )
 
 type Object interface {
@@ -95,3 +96,13 @@ func (f *Function) Inspect() string {
 }
 
 func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+
+type BuiltinFunction func(args ...Object) Object
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Inspect() string { return "builtin function" }
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
